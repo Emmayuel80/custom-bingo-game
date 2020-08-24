1034e68c7c49b8c39c451f99a6a81c404e64d194
Silvestre <emmayuel80@gmail.com>
2020-08-23 21:23:05 -0500

    make random numbers using string array

---
+++ services/getRandomNumbers.js
@@ -0,0 +1,12 @@
+const seedrandom = require('seedrandom');
+const customList = require('../config/customList')
+
+module.exports = function (arr) {
+    let randomNumbers = [];
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        seedrandom(element, {global: true});
+        randomNumbers.push(Math.floor(Math.random() * (customList.length - 0 + 1)) + 0);
+    }
+    return randomNumbers;
+}
