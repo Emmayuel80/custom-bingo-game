04976314ae73e2263b299f47fda973109421a58a
Silvestre <emmayuel80@gmail.com>
2020-08-24 22:02:33 -0500

    updated directory of mocha tests

--- test/test.js
+++ test/test.js
@@ -1,20 +1,20 @@
 const assert = require('assert')
-const generateCard = require('../services/generateCard')
+const generateCard = require('../server/services/generateCard')
 
 
 describe('#Divide String test', () => {
     it('Should throw an error if str is not a string', () => {
-        assert.throws(() =>{require('../services/divideString')(12312)}, Error)
+        assert.throws(() =>{require('../server/services/divideString')(12312)}, Error)
     })
     it('Should throw an error if the string length is less than 125', () => {
-        assert.throws(() => {require('../services/divideString')('lksdajflkdsa')}, Error)
+        assert.throws(() => {require('../server/services/divideString')('lksdajflkdsa')}, Error)
     })
     it('Should return an array with length of 25', () => {
-        let arr = require('../services/divideString')('w8ZtkN0bp33ECYnm3YVXhjCmcHGiBe1FsoZByhaGPddcyPPN8EHMypyorwKtMAFfPlBItn27f6o1OLuWoVPaIy0J2oE4TYVUK1eoh0mzlsL95Si3UKPJhGySx24o8');
+        let arr = require('../server/services/divideString')('w8ZtkN0bp33ECYnm3YVXhjCmcHGiBe1FsoZByhaGPddcyPPN8EHMypyorwKtMAFfPlBItn27f6o1OLuWoVPaIy0J2oE4TYVUK1eoh0mzlsL95Si3UKPJhGySx24o8');
         assert.strictEqual(arr.length, 25)
     })
     it('Should return an array in which its elements are strings with a length of 5', () => {
-        let arr = require('../services/divideString')('w8ZtkN0bp33ECYnm3YVXhjCmcHGiBe1FsoZByhaGPddcyPPN8EHMypyorwKtMAFfPlBItn27f6o1OLuWoVPaIy0J2oE4TYVUK1eoh0mzlsL95Si3UKPJhGySx24o8');
+        let arr = require('../server/services/divideString')('w8ZtkN0bp33ECYnm3YVXhjCmcHGiBe1FsoZByhaGPddcyPPN8EHMypyorwKtMAFfPlBItn27f6o1OLuWoVPaIy0J2oE4TYVUK1eoh0mzlsL95Si3UKPJhGySx24o8');
         assert.strictEqual(arr[0].length, 5)
     })
 })
@@ -28,7 +28,7 @@ describe('#Card generation Tests', () => {
 
 describe('#Random string generation Tests', () => {
     it('Should return an string with length equal to 125', () => {
-        let str = require('../services/generateRandomString')(125);
+        let str = require('../server/services/generateRandomString')(125);
         assert.strictEqual(str.length, 125)
     })
 })
