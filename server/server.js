be186122fc5e9d99b44e6cc29a165dbaf725b54e
Silvestre <emmayuel80@gmail.com>
2020-08-24 21:38:42 -0500

    update paths

--- server/server.js
+++ server/server.js
@@ -14,9 +14,11 @@ app.use(logger('dev'));
 //ROUTES
 app.use('/api', routes)
 
+//MIDDLEWARE
+app.use(express.static(path.join(__dirname, "../client", "build")))
 //SERVER
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
 });
 
 app.listen(PORT,function(){
