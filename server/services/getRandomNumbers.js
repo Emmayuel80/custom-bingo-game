2ceafb997c0ac05fae55965422be9715d99b6e3d
Silvestre <emmayuel80@gmail.com>
2020-09-05 13:59:28 -0500

    fixed indexout of bounds error

--- server/services/getRandomNumbers.js
+++ server/services/getRandomNumbers.js
@@ -6,7 +6,7 @@ module.exports = function (arr) {
     for (let i = 0; i < arr.length; i++) {
         const element = arr[i];
         seedrandom(element, {global: true});
-        randomNumbers.push(Math.floor(Math.random() * (customList.length - 0 + 1)) + 0);
+        randomNumbers.push(Math.floor(Math.random() * ((customList.length-1) - 0 + 1)) + 0);
     }
     return randomNumbers;
 }
